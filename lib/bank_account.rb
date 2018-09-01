eb82641a5f15cd12873c8a728024de9feedaf391
drubaby <peryton6@gmail.com>
2018-09-01 16:11:54 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+  # one instance of the class can transfer money to another instance through a Transfer class
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
